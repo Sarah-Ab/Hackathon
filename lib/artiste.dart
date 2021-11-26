accb818a6feb6d7b1c93cda2327064d6f99a260d
anthonyamiard <anthony.amiard@outlook.fr>
2021-11-26 14:36:43 +0100

    Création classe Projet

--- lib/artiste.dart
+++ lib/artiste.dart
@@ -1,11 +1,17 @@
 import 'package:hackathon/edition.dart';
+import 'package:hackathon/projet.dart';
 
 class Artiste {
   String nom;
   Edition edition;
+  List<Projet> projets;
   String? spotify;
   String? deezer;
 
   Artiste(
-      {required this.nom, required this.edition, this.spotify, this.deezer});
+      {required this.nom,
+      required this.edition,
+      required this.projets,
+      this.spotify,
+      this.deezer});
 }
---
+++ lib/projet.dart
@@ -0,0 +1,9 @@
+class Projet {
+  String nom;
+  DateTime date;
+  String salle;
+  String? ville;
+
+  Projet(
+      {required this.nom, required this.date, required this.salle, this.ville});
+}
