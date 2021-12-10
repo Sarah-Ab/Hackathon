41a88c58da3c0604357bea0c3a33cfbfc7159932
sarah_labistic <s.ababou@free.fr>
2021-12-10 13:44:13 +0100

    notif

--- lib/mobile/widgets/custom_app_barmobile.dart
+++ lib/mobile/widgets/custom_app_barmobile.dart
@@ -15,37 +15,54 @@ class CustomAppBarMobile extends StatefulWidget implements PreferredSizeWidget {
 
 class _CustomAppBarMobileState extends State<CustomAppBarMobile> {
   bool _nouvelleNotif = false;
-  @override
-  initState() {
-    super.initState();
-    Future<NotificationService> notif = NotificationService.instance;
-    notif.then((value) => value.onNotification((p0) {
-          setState() {
-            _nouvelleNotif = true;
-          }
-        }));
-  }
-
   bool selected = true;
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text('Bienvenue sur Transmusicale'),
+
       actions: [
         Container(
           padding: const EdgeInsets.all(8.0),
-          child: IconButton(
-            icon: Icon(_nouvelleNotif
-                ? Icons.notification_add
-                : Icons.notifications_off),
-            onPressed: () {
-              setState(() {
-                selected = !selected;
-              });
+          child: FutureBuilder(
+            future: NotificationService.instance,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return test(snapshot.data as NotificationService);
+              } else if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              } else {
+                return const Text("attente");
+              }
             },
-          ),
+          )
         ),
       ],
     );
   }
 }
+  class test extends StatefulWidget{
+  NotificationService n;
+  test(this.n);
+  @override
+  State<StatefulWidget> createState() => test0();
+  }
+
+  class test0 extends State<test>{
+  bool no = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    widget.n.onNotification((p0) {
+      setState(() {
+        no = true;
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Icon(no? Icons.notification_important_rounded : Icons.ac_unit_outlined);
+  }
+
+  }
